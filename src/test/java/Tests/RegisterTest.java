dbdc95ff364140904d7d688d940ae177aea80229
RazGav <razvu98@gmail.com>
2021-12-23 19:44:43 +0200

    finalizare register

--- src/test/java/Tests/RegisterTest.java
+++ src/test/java/Tests/RegisterTest.java
@@ -43,46 +43,101 @@ public class RegisterTest {
         String lastName = "Bala";
         lastNameElement.sendKeys(lastName);
 
+        //Identificam elemntul Adresa
+        WebElement adressElement = driver.findElement(By.xpath("//textarea[@ng-model='Adress']"));
+        String adresa = "Str. Ajun, nr. 1, bloc D12, Ap.24";
+        adressElement.sendKeys(adresa);
+
+        //Identificam elementul Email
+        WebElement emailElement = driver.findElement(By.cssSelector("input[ng-model=\"EmailAdress\"]"));
+        String email = "portocala@gmail.ru";
+        emailElement.sendKeys(email);
+
+        //Identificam elementul "Phone"
+        WebElement phoneElement = driver.findElement(By.cssSelector("input[type=\"tel\"]"));
+        String phoneNumber = "0763298431";
+        phoneElement.sendKeys(phoneNumber);
+
+        //Identificam elementul radiobutton Male
+        WebElement radiobuttonMaleElement = driver.findElement(By.cssSelector("input[value=\"Male\"]"));
+        radiobuttonMaleElement.click();
+
         //Identificam elementul radiobutton Female
         WebElement radiobuttonFemaleElement = driver.findElement(By.cssSelector("input[value=\"FeMale\"]"));
         radiobuttonFemaleElement.click();
 
         //Identificam "Cricket" element
-        WebElement checkboxCricket = driver.findElement(By.id("checkbox1"));
-        checkboxCricket.click();
+        WebElement checkboxCricketElement = driver.findElement(By.id("checkbox1"));
+        checkboxCricketElement.click();
+
+        //Identificam "Movies" element
+        WebElement checkboxMoviesElement = driver.findElement(By.id("checkbox2"));
+        checkboxMoviesElement.click();
+
+        //Identificam "Hockey" element
+        WebElement checkboxHockeyElement = driver.findElement(By.id("checkbox3"));
+        checkboxHockeyElement.click();
 
         //Identificam Skills dropbox element
         WebElement skillsDropboxElement = driver.findElement(By.id("Skills"));
         Select skillsSelect = new Select(skillsDropboxElement);
         skillsSelect.selectByVisibleText("Android");
 
+        //Identificam Select Country dropbox element
+        WebElement selectCountryDropboxElement = driver.findElement(By.id("country"));
+        Select selectCountry = new Select(selectCountryDropboxElement);
+        selectCountry.selectByVisibleText("Denmark");
+
         //Identificam Year dropbox element
         WebElement yearDropboxElement = driver.findElement(By.id("yearbox"));
         Select yearSelect = new Select(yearDropboxElement);
         yearSelect.selectByValue("1998");
 
+        //Identificam Month dropbox element
+        WebElement monthDropboxElement = driver.findElement(By.xpath("//select[@ng-model='monthbox']"));
+        Select monthSelect = new Select(monthDropboxElement);
+        monthSelect.selectByVisibleText("March");
+
+        //Identificam Day dropbox element
+        WebElement dayDropboxElement = driver.findElement(By.id("daybox"));
+        Select daySelect = new Select(dayDropboxElement);
+        daySelect.selectByValue("14");
+
+        //Identificam Password element
+        WebElement passwordElement = driver.findElement(By.id("firstpassword"));
+        String password = "Parola1";
+        passwordElement.sendKeys(password);
+
+        //Identificam Password element
+        WebElement secondPasswordElement = driver.findElement(By.id("secondpassword"));
+        String password2 = "Parola1";
+        secondPasswordElement.sendKeys(password2);
+
         //Identificam fake dropbox Language element
         WebElement fakeLanguageDropboxElement = driver.findElement(By.id("msdd"));
         fakeLanguageDropboxElement.click();
-
         List<WebElement> languageElements = driver.findElements(By.xpath("//li[@class='ng-scope']/a"));
-        for(int i = 0; i< languageElements.size(); i++){
+        for(int i = 0; i < languageElements.size(); i++){
             if(languageElements.get(i).getText().equals("Bulgarian")){
                 languageElements.get(i).click();
             }
         }
 
-        WebElement imageElement = driver.findElement(By.id("imagetrgt"));
-        imageElement.click();
+        //Click pe imagine pentru a inchide lista de mai sus
+        //WebElement imageElement = driver.findElement(By.id("imagetrgt"));
+        //imageElement.click();
 
-        WebElement adressElement = driver.findElement(By.xpath("//textarea[@ng-model='Adress']"));
-        String adresa = "Str. Ajun, nr. 1, bloc D12, Ap.24";
-        adressElement.sendKeys(adresa);
+        //Upload photo
+        WebElement photoElement = driver.findElement(By.id("imagesrc"));
+        photoElement.sendKeys("C:\\Users\\razvu\\OneDrive\\Desktop\\Untitled.png");
 
-        //Identificam Year dropbox element
-        WebElement passwordElement = driver.findElement(By.id("firstpassword"));
-        String password = "Parola1";
-        passwordElement.sendKeys(password);
+        //Submit button
+        WebElement submitButtonElement = driver.findElement(By.id("submitbtn"));
+        submitButtonElement.click();
+
+        //Refresh button
+        //WebElement refreshButtonElement = driver.findElement(By.id("Button1"));
+        //refreshButtonElement.click();
 
 
         // "// = de unde incepi, [@ = dupa ce cauti], / = intri mai adanca in lista cautata pt xpath
